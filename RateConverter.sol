2eddc3e26bd45798dcdf12ce9efb0cca7161635d
George Karumbi <55401368+gkarumbi@users.noreply.github.com>
2024-12-05 13:28:46 +0300

    Create RateConverter.sol with skeleton code

---
+++ RateConverter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+//This contract convert checks ETH/USD rates and Vice Versa
+
+contract RateConverter{
+
+    function ethToUSD() public view {
+
+    }
+
+    function usdToEth() public view{
+        
+    }
+}
